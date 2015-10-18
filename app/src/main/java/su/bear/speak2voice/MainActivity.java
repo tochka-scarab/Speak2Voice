1facf625006b64093da681262a7a141ac27fa158
tochka-scarab <tochka.scarab@gmail.com>
2015-10-18 18:08:28 +0300

    Add txtText and btnSpeak

--- app/src/main/java/su/bear/speak2voice/MainActivity.java
+++ app/src/main/java/su/bear/speak2voice/MainActivity.java
@@ -8,9 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private ImageButton btnSpeak;
+    private TextView txtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,6 +35,11 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
+        txtText = (TextView) findViewById(R.id.txtText);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+
+
     }
 
     @Override
